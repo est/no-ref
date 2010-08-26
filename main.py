4ef066631fbb3fa317499d3b7f91bfc6ff3c1b23
est <electronicstar@126.com>
2010-08-26 15:24:52 +0800

    'typo'

---
+++ main.py
@@ -0,0 +1,52 @@
+﻿#!/usr/bin/env python
+#coding: utf-8
+
+
+
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from urllib import unquote as url_unquote
+import re, os, logging
+
+class homepage(webapp.RequestHandler):
+
+    def get(self):
+        self.response.out.write(open('index.html').read())
+
+class gfw(webapp.RequestHandler):
+    pass
+
+class hotlink_img(webapp.RequestHandler):
+    pass
+
+class jump(webapp.RequestHandler):
+    
+    def get(self):
+        url=self.request.url
+        r=re.search(r'^http://(.*)$', url)
+        is_dev = os.environ['SERVER_SOFTWARE'][:3]
+        if r and is_dev!='Dev':
+            self.redirect('https://'+r.groups()[0], permanent=True)
+        else:
+            r=re.search(r'http.*(http.?://.*$)', url_unquote(url))
+            if r:
+                self.redirect(r.groups()[0], permanent=True)
+            else:
+                self.response.set_status(400)
+                self.response.out.write(url)
+
+
+def main():
+    application = webapp.WSGIApplication([
+        ('/', homepage),
+        ('/gfw(.*)', gfw),
+        # TODO: more features
+        ('/hotlink_demo.jpg', hotlink_img),
+        ('.*', jump),
+        ], debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
